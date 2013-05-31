fd8ded68e1705b8f3de1aa2a0e1714877fa65ad4
Solkin <inbox@tomclaw.com>
2013-05-31 10:53:06 +0400

    Some architecture updates

--- src/com/tomclaw/mandarin/core/SessionHolder.java
+++ src/com/tomclaw/mandarin/core/SessionHolder.java
@@ -23,12 +23,13 @@ import java.util.List;
  */
 public class SessionHolder {
 
-    private final String ACCOUNT_DATA = "account_data";
-    private final String ACCOUNT_TYPE = "account_type";
+    private static final String ACCOUNT_DATA = "account_data";
+    private static final String ACCOUNT_TYPE = "account_type";
 
     private List<AccountRoot> accountRootList = new ArrayList<AccountRoot>();
     private final ContentResolver contentResolver;
     private final AccountManager accountManager;
+    private static Gson gson;
 
     public SessionHolder(Context context) {
         accountManager = AccountManager.get(context);
@@ -39,14 +40,18 @@ public class SessionHolder {
             @Override
             public void onAccountsUpdated(Account[] accounts) {
                 Log.d(Settings.LOG_TAG, "SessionHolder: onAccountsUpdated");
+                // Отсюда могут поступать только в уменьшенном количестве.
                 update(accounts);
             }
         }, null, false);
+        gson = new Gson();
     }
 
     public void load() {
         Log.d(Settings.LOG_TAG, "SessionHolder: load");
         // Loading accounts from local storage.
+        // Если мы что-то храним в базе, то оно должно обновляться отсюда.
+        // Но не путём перезаписи, а поиском разницы для удаления того, чего уже нет.
         Account[] accounts = accountManager.getAccountsByType(AccountAuthenticator.ACCOUNT_TYPE);
         update(accounts);
     }
@@ -57,14 +62,30 @@ public class SessionHolder {
      */
     public void update(Account[] accounts) {
         // Clear and fill up with updated list.
-        accountRootList.clear();
+        // Поиск разницы.
+        // accountRootList.clear();
+        List<AccountRoot> accountsList = accountRootList;
         for(Account account : accounts) {
             // Checking for account type.
             if(account.type.equals(AccountAuthenticator.ACCOUNT_TYPE)) {
                 // Obtain account clone from account manager.
                 AccountRoot accountRoot = getAccountRoot(account);
-                accountRootList.add(accountRoot);
-                // Query fot such account in database.
+                // accountRootList.add(accountRoot);
+                for(AccountRoot localAccount : accountsList) {
+                    // Checking for equals.
+                    if(compare(accountRoot, localAccount)) {
+                        // Такая учётная запись уже имеется.
+                        accountRoot = null;
+                        // Query to add account.
+                        addAccountLocal(localAccount);
+
+                    }
+                }
+                // Нашёл ли кто учётку?
+                if(accountRoot != null) {
+                    addAccountRoot(accountRoot);
+                }
+                // Query for such account in database.
                 // if(QueryHelper.updateAccount(contentResolver, accountRoot)) {
                     // Account was changed. Account must be offline now. Switch it.
                 // }
@@ -81,14 +102,17 @@ public class SessionHolder {
         final Account account = new Account(accountRoot.getUserId(), AccountAuthenticator.ACCOUNT_TYPE);
         if(accountManager.addAccountExplicitly(account, accountRoot.getUserPassword(), getAccountData(accountRoot))) {
             // Query to add account.
-            accountRootList.add(accountRoot);
+            addAccountLocal(accountRoot);
         }
     }
 
-    private Bundle getAccountData(AccountRoot accountRoot) {
+    private void addAccountLocal(AccountRoot accountRoot) {
+        accountRootList.add(accountRoot);
+    }
+
+    private static Bundle getAccountData(AccountRoot accountRoot) {
         Bundle bundle = new Bundle();
         // Writing json data.
-        Gson gson = new Gson();
         String accountData = gson.toJson(accountRoot);
         Log.d(Settings.LOG_TAG, "account data: " + accountData);
         bundle.putString(ACCOUNT_DATA, accountData);
@@ -101,7 +125,6 @@ public class SessionHolder {
         String accountData = accountManager.getUserData(account, ACCOUNT_DATA);
         String accountType = accountManager.getUserData(account, ACCOUNT_TYPE);
         Log.d(Settings.LOG_TAG, "account [" + accountType + "] data: " + accountData);
-        Gson gson = new Gson();
         AccountRoot accountRoot = null;
         try {
             accountRoot = (AccountRoot) gson.fromJson(accountData, Class.forName(accountType));
@@ -109,5 +132,14 @@ public class SessionHolder {
             Log.d(Settings.LOG_TAG, "Unknown account class type: " + accountType);
         }
         return accountRoot;
+    }
+
+    private static boolean compare(AccountRoot accountRoot1, AccountRoot accountRoot2) {
+        if(accountRoot1.getClass().equals(accountRoot2.getClass())) {
+            if(gson.toJson(accountRoot1).equals(gson.toJson(accountRoot2))) {
+                return true;
+            }
+        }
+        return false;
     }
 }

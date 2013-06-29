8d62a5aa7f9660243659254fcda255af8b2a445d
Igor Solkin <inbox@tomclaw.com>
2013-06-30 01:47:28 +0400

    Minor changes

--- src/com/tomclaw/mandarin/main/adapters/ChatPagerAdapter.java
+++ src/com/tomclaw/mandarin/main/adapters/ChatPagerAdapter.java
@@ -12,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import com.astuetz.viewpager.extensions.PagerSlidingTabStrip;
 import com.tomclaw.mandarin.R;
 import com.tomclaw.mandarin.core.GlobalProvider;
 import com.tomclaw.mandarin.core.Settings;

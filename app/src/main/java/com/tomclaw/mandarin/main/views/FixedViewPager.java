583d2f54ef7a9f6d657a285d631d35b77351afc7
Igor Solkin <inbox@tomclaw.com>
2016-08-28 16:17:06 +0300

    Исправление ошибки во ViewPager: https://code.google.com/p/android/issues/detail?id=18990

--- app/src/main/java/com/tomclaw/mandarin/main/views/FixedViewPager.java
+++ app/src/main/java/com/tomclaw/mandarin/main/views/FixedViewPager.java
@@ -5,6 +5,8 @@ import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
+import com.tomclaw.mandarin.util.Logger;
+
 /**
  * Created by Solkin on 26.12.2014.
  */
@@ -19,8 +21,23 @@ public class FixedViewPager extends ViewPager {
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (IllegalArgumentException ignored) {
+            Logger.log("Some strange error in ViewPager (onTouchEvent)", ignored);
+        }
+        return false;
+    }
+
+    @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        // Prevent NPE if fake dragging and touching ViewPager.
-        return !isFakeDragging() && super.onInterceptTouchEvent(ev);
+        try {
+            // Prevent NPE if fake dragging and touching ViewPager.
+            return !isFakeDragging() && super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException ignored) {
+            Logger.log("Some strange error in ViewPager (onInterceptTouchEvent)", ignored);
+        }
+        return false;
     }
 }
